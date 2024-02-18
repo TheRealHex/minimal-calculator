143bd1cb35dc4e7aeafe5db6c773f6a6fe532e25
TheRealHex <devilxnp@gmail.com>
2024-02-18 08:58:15 +0545

    append values into variables

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:minimal_calculator/constants/btn_values.dart';
 
 import '../constants/style.dart';
-import '../functions.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -61,5 +60,63 @@ class _HomeState extends State<Home> {
     );
   }
 
+// void calculate() {}
+// void percentage() {}
+// void delete() {}
+// void clear() {}
+
 //#### functions
+  void appendValue(String value) {
+    // If value is only operand & not dot
+    if (value != Btn.dot && int.tryParse(value) == null) {
+      operand = value;
+    }
+    // for num1
+    else if (num1.isEmpty || operand.isEmpty) {
+      // check value is dot (1.4, 1.45)
+      if (value == Btn.dot && num1.contains(Btn.dot)) return;
+      // put 0. if dot is pressed at first
+      if (value == Btn.dot && (num1.isEmpty || num1 == Btn.dot)) {
+        value = '0.';
+      }
+      num1 += value;
+    }
+    // for num2
+    else if (num2.isEmpty || operand.isNotEmpty) {
+      // check value is dot (1.4, 1.45)
+      if (value == Btn.dot && num2.contains(Btn.dot)) return;
+      // put 0. if dot is pressed at first
+      if (value == Btn.dot && (num2.isEmpty || num2 == Btn.dot)) {
+        value = '0.';
+      }
+      num2 += value;
+    }
+
+    setState(() {});
+  }
+
+  // on button press
+  void onBtnTap(String value) {
+    appendValue(value); // to append input at the end
+  }
+
+// Function to build button
+  Widget buildBtn(value) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: [Btn.del, Btn.clr, Btn.calc].contains(value)
+                ? Colors.redAccent
+                : [Btn.per, Btn.mul, Btn.div, Btn.add, Btn.sub].contains(value)
+                    ? Colors.red
+                    : null),
+        onPressed: () => onBtnTap(value),
+        child: Text(
+          value,
+          style: inputTextStyle,
+        ),
+      ),
+    );
+  }
 }
